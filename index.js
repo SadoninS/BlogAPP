1ceaa9f1a6e8f1e0a8ad184888f2a84c22a4a6ef
SadoninS <stanislavsadonin@gmail.com>
2023-06-10 15:44:15 +0400

    refactor index.js stage1

--- index.js
+++ index.js
@@ -45,34 +45,6 @@ const post = {
   text: '',
 };
 
-function getPostFromUser(postTitleFromUser, postTextFromUser) {
-  const dt = new Date();
-  const dtOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  };
-  post.date = dt.toLocaleString('ru', dtOptions);
-  post.title = postTitleFromUser.value.trim();
-  post.text = postTextFromUser.value.trim();
-
-  return post;
-}
-
-function emptyStringValidation(post) {
-  return post.title !== '' && post.text !== '';
-}
-
-function lengthValidation(post) {
-  return post.title.length <= 50 && post.text.length <= 1000;
-}
-
-function validationPassed(post) {
-  return lengthValidation(post) && emptyStringValidation(post);
-}
-
 postTitleFromUser.addEventListener('input', function () {
   inputErrorMessage.classList.add('invisible');
   if (postTitleFromUser.value.length >= 0) {
@@ -99,42 +71,8 @@ postTextFromUser.addEventListener('input', function () {
   }
 });
 
-function createPost(post) {
-  postsList.push(
-    (postsNode.innerHTML = `<div class="post">
-                          <p class='post-date'>${post.date}</p>
-                          <h3 class='post-title'>${post.title}</h3>
-                          <p class='post-text'>${post.text}</p>
-                        </div>`)
-  );
-  const postListStorage = JSON.stringify(postsList);
-  localStorage.setItem('postlist', postListStorage);
-  postTitleFromUser.value = '';
-  postTextFromUser.value = '';
-  postListClearBtnNode.classList.remove('invisible');
-}
-
 function renderPosts(postsList) {
   postsNode.innerHTML = postsList.join('');
-}
-function resetValidationMessages(
-  postTitleValidationMessage,
-  postTextValidationMessage
-) {
-  postTitleValidationMessage.innerText = '0/50';
-  postTextValidationMessage.innerText = '0/1000';
-  postTitleValidationMessage.classList.remove('overLength');
-  postTextValidationMessage.classList.remove('overLength');
-}
-
-function showSubmitErrorMessage(inputErrorMessage) {
-  if (!validationPassed(post)) {
-    inputErrorMessage.classList.remove('invisible');
-  }
-}
-
-function hideErrorMessage(inputErrorMessage) {
-  inputErrorMessage.classList.add('invisible');
 }
 
 publishBtn.addEventListener('click', function () {
